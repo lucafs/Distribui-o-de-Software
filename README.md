3b57ca9eaddd703f80d27ad91e205796c043608a
lucafs@al.insper.edu.br <lucafs@al.insper.edu.br>
2021-09-21 13:33:26 -0700

    Pypi deploy

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Distribuição de Software
 
-This is a test package
+Pacote teste para disciplina dev-aberto.
 
 [**Repositório da disciplina**](https://github.com/Insper/dev-aberto)
+
+[**Pypi Link**](https://test.pypi.org/project/dev-aberto-lucafs/0.1/)
--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
 from setuptools import setup
 
 setup(name='dev_aberto_lucafs',
-      version='0.1',
-      packages=['dev_aberto']
+      version = '0.1',
+      packages = ['dev_aberto'],
+      author = 'Luca Farah',
+      plataforms = ['Windowns', 'Linux' , 'Unix' , 'MAC OS'],
+      python_requires=">=3.6",
+      install_requires=["requests"],
+      scripts=['scripts/hello.py']
       )

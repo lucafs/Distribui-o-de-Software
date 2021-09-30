db57afe470cb029ec8a1ad1ca2b43b33beed1880
lucafs@al.insper.edu.br <lucafs@al.insper.edu.br>
2021-09-30 08:26:10 -0700

    translation done

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+from babel.dates import format_datetime
+import gettext
+
+
+gettext.install('cli',localedir='locale')
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    date = format_datetime()
+    print(_('Último commit feito em:'), date, _(' por'), name)
